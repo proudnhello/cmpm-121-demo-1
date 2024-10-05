728eea828e6411eba87fedc67ab8db17f834f8e9
proudnhello <92887967+proudnhello@users.noreply.github.com>
2024-10-05 15:53:48 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,7 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "Recruit a soldier<br> 🔫"; // I wanted to use, which is a saluting face, but its not supported on windows 10, and idk if its working.
+app.append(button);
