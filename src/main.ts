c26d29682b864965d68237945f778a4e4922f3c6
proudnhello <92887967+proudnhello@users.noreply.github.com>
2024-10-05 20:41:56 -0700

    step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -17,18 +17,28 @@ app.append(counter);
 
 // Button that increases the amount of soldiers.
 const button = document.createElement("button");
-button.innerHTML = "Recruit a soldier<br> 🔫"; // I wanted to use, which is a saluting face, but its not supported on windows 10, and idk if its working.
+button.innerHTML = "Recruit a soldier<br> 🔫"; // I wanted to use a saluting face, but its not supported on windows 10.
 app.append(button);
 
 let soldiers = 0;
 
 button.addEventListener("click", increaseSoldiers.bind(null, 1));
 
-setInterval(increaseSoldiers.bind(null, 1), 1000);
+// Loop code inspired by https://developer.mozilla.org/en-US/docs/Web/API/Window/requestAnimationFrame
+
+requestAnimationFrame(gameLoop.bind(null, Date.now()));
+
+function gameLoop(previousTime: number) {
+    const currentTime = Date.now();
+    const elapsed = currentTime - previousTime;
+
+    increaseSoldiers(elapsed / 1000);
+
+    requestAnimationFrame(gameLoop.bind(null, currentTime));
+}
 
 // Increaces the soldiers by the amount, then updates the counter.
 function increaseSoldiers(amount: number) {
   soldiers += amount;
   counter.innerHTML = `Soldiers: ${soldiers} <br>`;
 }
-
