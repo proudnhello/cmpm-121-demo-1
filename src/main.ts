6b47ef195590da00ebd8be3be8275a5509eaa5d0
proudnhello <92887967+proudnhello@users.noreply.github.com>
2024-10-05 20:21:45 -0700

    step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,30 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Army Builder";
 document.title = gameName;
 
+// Title
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-let counter = document.createElement("div");
+// Counter that lists the amount of soldiers.
+const counter = document.createElement("div");
 counter.innerHTML = "Soldiers: 0 <br>";
 app.append(counter);
 
+// Button that increases the amount of soldiers.
 const button = document.createElement("button");
 button.innerHTML = "Recruit a soldier<br> 🔫"; // I wanted to use, which is a saluting face, but its not supported on windows 10, and idk if its working.
 app.append(button);
 
 let soldiers = 0;
 
+button.addEventListener("click", increaseSoldiers.bind(null, 1));
 
-button.addEventListener("click", () => {
-    soldiers++;
-    counter.innerHTML = `Soldiers: ${soldiers} <br>`;
-});
+setInterval(increaseSoldiers.bind(null, 1), 1000);
+
+// Increaces the soldiers by the amount, then updates the counter.
+function increaseSoldiers(amount: number) {
+  soldiers += amount;
+  counter.innerHTML = `Soldiers: ${soldiers} <br>`;
+}
+
