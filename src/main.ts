961e18834fbff4871b053a0a7190dc5abbea185b
proudnhello <92887967+proudnhello@users.noreply.github.com>
2024-10-05 20:11:15 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,25 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My actually quite boring game";
+const gameName = "Army Builder";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+let counter = document.createElement("div");
+counter.innerHTML = "Soldiers: 0 <br>";
+app.append(counter);
+
 const button = document.createElement("button");
 button.innerHTML = "Recruit a soldier<br> 🔫"; // I wanted to use, which is a saluting face, but its not supported on windows 10, and idk if its working.
 app.append(button);
+
+let soldiers = 0;
+
+
+button.addEventListener("click", () => {
+    soldiers++;
+    counter.innerHTML = `Soldiers: ${soldiers} <br>`;
+});
